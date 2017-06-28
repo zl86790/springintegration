e179dd6f2f742ff8430de7d02312c4b6c7d633ce
Lizhe <Lizhe@Lizhe-PC>
2017-06-28 18:30:22 +0800

    added to rabbitmq

--- springintegration/src/main/java/name/lizhe/channeladapter/rabbitmq/App.java
+++ springintegration/src/main/java/name/lizhe/channeladapter/rabbitmq/App.java
@@ -2,6 +2,9 @@ package name.lizhe.channeladapter.rabbitmq;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 
 /**
  * Main entry-point into the test application
@@ -11,6 +14,9 @@ public class App {
 	public static void main(String args[]) {
 		String cfg = "name/lizhe/channeladapter/rabbitmq/applicationContext.xml";
 		ApplicationContext context = new ClassPathXmlApplicationContext(cfg);
+		MessageChannel channel = context.getBean("toRabbit", MessageChannel.class);
+		Message<String> message = MessageBuilder.withPayload("World").build();
+		channel.send(message);
 	}
 
 }
